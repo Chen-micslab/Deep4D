81a54347e466e6567d54ea76654877f48b2e915c
Chen-micslab <79675347+Chen-micslab@users.noreply.github.com>
2023-09-06 14:48:02 +0800

    Update predict_charge.py

--- Charge_state/predict_charge.py
+++ Charge_state/predict_charge.py
@@ -46,7 +46,7 @@ def get_position_angle_vec(position,dim):
 def get_mask(peptide,length):
     mask = torch.zeros(peptide.size(0),peptide.size(1))
     for i in range(length.size(0)):
-        mask[i, :int(length[i])] = 1
+        mask[i, :int(length[i])] = 0
     return  mask
 
 def predict_with_label(args):
